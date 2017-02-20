ecc5d2db790f1a291c157ec119df42f448bcd17a
Alex Sinfarosa <as898@cornell.edu>
2017-02-20 16:32:10 -0500

    updates

--- src/components/SelectionPanel/StationSelector.js
+++ src/components/SelectionPanel/StationSelector.js
@@ -15,7 +15,7 @@ class StationSelector extends Component {
     return (
       <div>
         <label className="label">Select a Station:
-        <span className="primary-color">{getFilteredStations.length}</span></label>
+        <span style={{marginLeft: '3px', color: '#3273DC'}}>{getFilteredStations.length}</span></label>
         <div className="control">
           <span className="select">
             <select
